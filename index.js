6778ee405014de1334615aab93e14acd0d87498f
Laud Bentil <laudbentil@gmail.com>
2019-11-19 21:22:05 +0000

    update name, update endpoint

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
     const express    = require('express')
     const bodyParser = require('body-parser')
     const app        = express()
+    const PORT       = 3001
 
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
+
+    app.get('/', (req, res) => res.status(200).send({'message': 'Simple Attendance'}) )
+
+    app.listen( PORT, ()=> {
+        console.log(`Simple Attendance is live at ${PORT}`)
+    })
