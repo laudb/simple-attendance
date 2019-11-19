8d1d4d43e4a3a2e64f99392aed116be993cdebc6
Laud Bentil <laudbentil@gmail.com>
2019-11-19 20:56:24 +0000

    change app structure

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-(function () {
-    'use strict'
+    const express    = require('express')
+    const bodyParser = require('body-parser')
+    const app        = express()
 
-    const express = require('express')
-    const app     = express()
-})();
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
