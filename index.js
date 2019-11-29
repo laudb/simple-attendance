d77ef59df8e7dd554b46c6dd885cff602f8b118d
Laud Bentil <laudbentil@gmail.com>
2019-11-29 17:24:40 +0000

    update check-in endpoints

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
 
-    mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    mongoose.connect('mongodb://localhost:27017/attendeedb', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     const db = mongoose.connection;
 
     db.once('open', () => {
@@ -37,16 +37,18 @@
         const attendee = new Attendee({
             fullName, userEmail, checkInTime
         });
+            
+            attendee.save( (err) => {
+            
+                if (err) { 
+                    res.status(500).send({'response': ' Attendee not created'});
+                }
+            
+                // save information and return response
+                res.status(201).send({"response": `Welcome ${fullName}, Check In time is ${checkInTime}`});        
+            });
 
-        attendee.save((err, data) => {
-            if (err) { 
-                res.status(500).send({'response': ' Attendee not created'});
-            }
-
-            // save information and return response
-            res.status(201).send({"response": `Welcome ${fullName}, Check In time is ${checkInTime}`});        
-        })
-    })
+        });
 
     app.post('/check-out', (req, res) => {
 
