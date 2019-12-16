2c03833e529c05f7092c5291dc7dc578275ff5ef
Laud Bentil <laudbentil@gmail.com>
2019-12-16 10:15:09 +0000

    fix test runner issue

--- routes.test.js
+++ routes.test.js
@@ -1,13 +1,32 @@
 const request = require('supertest')
+const mongoose = require('mongoose')
 const app     = require('./index')
+let   server
+
+beforeAll(async (done) => {
+    await mongoose.connect(process.env.TESTMONGOURL)
+    server = app.listen(process.env.TESTPORT , () => {
+        global.agent = request.agent(server)
+        done();
+    });
+});
+
+afterAll(async () => {
+    await server.close();
+    await mongoose.disconnect();
+});
+
 
 describe('Sample Test', () => {
+
     it('should test that true === true', () => {
         expect(true).toBe(true)
     })
+
 })
 
 describe('Endpoints', () => {
+
     it('should get the main url', async () => {
         const res = await request(app)
         .get('/v1/')
@@ -15,11 +34,13 @@ describe('Endpoints', () => {
         expect(res.body).toHaveProperty('response')
     })
 
-    it('should get main url error', async () => {
+    it('should get main url error', async (done) => {
         const res = await request(app)
-        .get('/y1/')
+        .get('/y1')
         expect(res.statusCode).toEqual(404)
-        expect(res.body).toHaveProperty('Route Not Found.')
+        expect(res.body).toHaveProperty('response')
+        expect(res.body.response).toEqual('Route Not Found.')
+        done();
     })
 })
 
