958bdce8228f169c18485ff090f66db757d2fd35
Laud Bentil <laudbentil@gmail.com>
2019-12-18 15:09:22 +0000

    update user model

--- models/user.js
+++ models/user.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 
 const userSchema = mongoose.Schema({
-    userEmail: { type: String, unique: true, required: true },
-    userPassCode: { type: String, required: true }
-})
+    userEmail: { type: String, unique: true, required: true, minlength: 3 },
+    userPassCode: { type: String, required: true, minlength: 3 },
+    isAdmin: Boolean
+});
 
-const User = mongoose.model("User", userSchema )
+const User = mongoose.model("User", userSchema );
 
 module.exports = User;
